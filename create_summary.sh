e3514d7db05ee96e83e19a53efb64f51e6f956e8
juripfammatter <juri.pfammatter@gmail.com>
2025-02-10 12:29:16 +0100

    changed -r to -R and added LANG encoding for sed

--- create_summary.sh
+++ create_summary.sh
@@ -5,11 +5,15 @@ NEW_REPO="eth-dyn-prog-opt-cont"
 NEW_DESC_LONG="Dynamic Programming and Optimal Control"
 
 echo "copy repo"
-cp -ra ./example/summary_template/. $NEW_REL_PATH
-cp -ra ./example/main.tex $NEW_REL_PATH/src
+cp -Ra ./example/summary_template/. $NEW_REL_PATH
+cp -Ra ./example/main.tex $NEW_REL_PATH/src
 
 echo "rename new repo"
 mv "$NEW_REL_PATH.vscode/eth-repo-name.code-workspace" "$NEW_REL_PATH.vscode/$NEW_REPO.code-workspace"
+
+export LANG=C
+export LC_CTYPE=C
+
 find $NEW_REL_PATH -type f -print0 | xargs -0 sed -i '' -e "s/ETHZ Summary Description/$NEW_DESC_LONG/g"
 find $NEW_REL_PATH -type f -print0 | xargs -0 sed -i '' -e "s/eth-repo-name/$NEW_REPO/g"
 
